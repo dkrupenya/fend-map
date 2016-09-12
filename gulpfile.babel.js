cd9eb525a8289f9d3907f33dc8e864dc684a2bba
Dmitry Krupenya <dkrupenya@gmail.com>
2016-09-13 01:17:26 +0300

    add dist folder to project

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -48,10 +48,10 @@ gulp.task('lint', () =>
 // Optimize images
 gulp.task('images', () =>
   gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
+    // .pipe($.cache($.imagemin({
+    //   progressive: true,
+    //   interlaced: true
+    // })))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}))
 );
