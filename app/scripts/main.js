41ab80bcbc085da55dceaea18285a993b6fc661d
Dmitry Krupenya <dkrupenya@gmail.com>
2016-09-13 00:45:04 +0300

    add comments; remove console.log

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,20 +24,53 @@
   const app = {};
   let lastQueryLocation = new google.maps.LatLng({lat: 0, lng: 0});
 
+  /* CONSTANTS */
+  const GOOGLE_MAP_OPTIONS = {
+    zoom: 13,
+    center: new google.maps.LatLng(37.7703706, -122.3871226),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    streetViewControl: false
+  };
+
+  // google map markers
+  const G_MARKER = {
+    path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+    fillColor: '#607d8b',
+    fillOpacity: 0.7,
+    scale: 0.7,
+    strokeColor: '#607d8b',
+    strokeWeight: 3
+  };
+  const G_MARKER_SELECTED = {
+    path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+    fillColor: '#009688',
+    fillOpacity: 0.8,
+    scale: 0.8,
+    strokeColor: '#009688',
+    strokeWeight: 3
+  };
+
+
+  /* CONTROLLER */
   app.controller = {};
 
+  /* MODEL */
   app.model = {
-    map: null, // google map object
-    places: ko.observableArray([]), // places marked on the map
-    placesHash: new Map(),
-    markers: new WeakMap(),
-    selectedPlaces: new Set(),
+    map: null,                      // google map object
 
+    places: ko.observableArray([]), // main places storage
+    placesHash: new Map(),          // helps to search place from foursquare place id
+    markers: new WeakMap(),         // helps to search place from google marker
+    selectedPlaces: new Set(),      // places marked on the map todo not in use now
+
+    //Selected place
     placeInFocus: ko.observable(),
     isPlaceInFocusVisible: ko.observable(false),
     hideDetailsModal: () => {app.model.isPlaceInFocusVisible(false)},
 
-    isFailureModelVisible: ko.observable(false),
+    // error message
+    isFailureModalVisible: ko.observable(false),
     hideFailureModal: () => {app.model.isFailureModelVisible(false)},
 
     textFilter: ko.observable(''),
@@ -50,45 +83,48 @@
       return _.filter(places, place => place.name.toLowerCase().indexOf(text) !== -1 );
     }),
 
+    // show spinner?
     isLoading: ko.observable(false),
 
+    // click on a place in menu handler
     onClickPlace: onClickPalace,
+
+    // click on a map marker handler
     onClickMarker: onClickMarker,
   };
 
+  /**
+   *  create a map, add map listener
+   */
   app.controller.initApp = function initApp() {
     // init google map
-    const myOptions = {
-      zoom: 13,
-      center: new google.maps.LatLng(37.7703706, -122.3871226),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      streetViewControl: false
-    };
-    const map =  new google.maps.Map(document.getElementById('google-map'), myOptions);
+    const map =  new google.maps.Map(document.getElementById('google-map'), GOOGLE_MAP_OPTIONS);
     app.model.map = map;
 
     map.addListener('bounds_changed', _.throttle(app.controller.loadPlaces, 3000));
   };
 
+  /**
+   * load bars from 4square
+   */
   app.controller.loadPlaces = function loadPlaces() {
     const map = app.model.map;
-    console.log('bounds_changed', map);
 
     // dont ask 4square on big areas
     if (map.zoom < 16) return;
-
-    const lat = map.center.lat(),
-          lng = map.center.lng();
 
     //distance between this point and last request
     let distance = google.maps.geometry.spherical.computeDistanceBetween(lastQueryLocation, map.center);
     // don't load new data if point is too close to previous search
     if (distance < 1000) return;
     lastQueryLocation = map.center;
-    console.log('lat', lat, 'lng', lng, 'dist', distance);
+    app.model.isLoading(true);
+
+    // load data from foursquare
     //TODO idea, calculate radius from map zoom
-    const requestOptions = {
+    const lat = map.center.lat(),
+          lng = map.center.lng();
+    const FoursquareRequestOptions = {
       ll: lat + ',' + lng,
       radius: 2000,
       section: 'drinks',
@@ -96,37 +132,41 @@
       client_secret: 'T4O0ZURMG00IGUTU4NKSQZ4DH0E5LGLMDAE20OJWPXMBD10Y',
       v: 20160909
     };
-    app.model.isLoading(true);
-    $.get('https://api.foursquare.com/v2/venues/explore', requestOptions, app.controller.addPlaces)
+    $.get('https://api.foursquare.com/v2/venues/explore', FoursquareRequestOptions, app.controller.addPlaces)
       .fail(function(){
-        //todo say user smth
         app.model.isFailureModelVisible(true);
       })
       .always(function() {
-        console.log('finish');
         app.model.isLoading(false);
       });
   };
 
+
+  /**
+   * add places to the map
+   * @param res - results from foursquare request
+   */
   app.controller.addPlaces = function(res) {
     //app.model.places.removeAll();
-    console.log('4 square', res);
-    console.log('4 square Items: ', res.response.groups[0].items);
+
     res.response.groups[0].items.forEach(function(item) {
-      if(app.model.placesHash.has(item.venue.id)) return; // don't allow to repeat items
+      if(app.model.placesHash.has(item.venue.id)) return; // don't allow to double items on the map
 
       const place = new Place(item);
       app.model.places.push(place);
-      console.log('item.venue.id', item.venue.id);
       app.model.placesHash.set(item.venue.id, place);
       app.model.markers.set(place.marker, place);
 
     });
-    console.log('after add', app.model.places());
     app.model.isLoading(false);
   };
 
 
+  /**
+   * map place
+   * @param item - one place from foursquare API response
+   * @constructor
+   */
   function Place(item) {
     const map = app.model.map;
     const lat = item.venue.location.lat,
@@ -145,57 +185,47 @@
     this.marker.addListener('click', onClickMarker);
   }
 
+  /**
+   * user clicks on a place in the menu
+   * @param place
+   */
   function onClickPalace(place) {
-    console.log('click place', place);
     if (app.model.selectedPlaces.has(place)) return;
+
     // remove all selected
     for (let selected of app.model.selectedPlaces) {
-      console.log('selected', selected);
       selected.isSelected(false);
       selected.marker.setIcon(G_MARKER);
       app.model.selectedPlaces.delete(selected);
     }
+
     //add place to selected
     place.isSelected(true);
     app.model.selectedPlaces.add(place);
+
+    // change marker icon and show place details modal window
     place.marker.setIcon(G_MARKER_SELECTED);
     app.model.placeInFocus(place);
     app.model.isPlaceInFocusVisible(true);
 
     app.model.map.panTo(place.location);
 
-    // hide side menu on small screens
+    // hide side menu on small screens after click
     if(window.matchMedia('(max-width: 426px)').matches) {
       $('.mdl-layout__drawer').removeClass('is-visible');
       $('.mdl-layout__obfuscator').removeClass('is-visible');
     }
   }
 
+  /**
+   * user clicks on a map marker
+   */
   function onClickMarker() {
-    console.log('marker click', this);
     const place = app.model.markers.get(this);
     onClickPalace(place);
   }
 
-  const G_MARKER = {
-    path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
-    fillColor: '#607d8b',
-    fillOpacity: 0.7,
-    scale: 0.7,
-    strokeColor: '#607d8b',
-    strokeWeight: 3
-  };
-
-  const G_MARKER_SELECTED = {
-    path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
-    fillColor: '#009688',
-    fillOpacity: 0.8,
-    scale: 0.8,
-    strokeColor: '#009688',
-    strokeWeight: 3
-  };
-
-
+  // init app after loading page
   $(function() {
     ko.applyBindings(app.model);
     app.controller.initApp();
